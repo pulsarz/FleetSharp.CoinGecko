af9e0f65fa8906fe4deca9173316688182634f91
pulsarz <nicksasa@gmail.com>
2023-04-21 10:43:44 +0200

    Add project files.

---
+++ FleetSharp.CoinGecko/CoinGecko.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Json;
+
+namespace FleetSharp.CoinGecko
+{
+    internal class CoinGeckoErgoPriceClass
+    {
+        public double? usd { get; set; }
+    }
+    internal class CoinGeckoErgoPriceResult
+    {
+        public CoinGeckoErgoPriceClass? ergo { get; set; }
+    }
+
+    public static class CoinGecko
+    {
+        private static HttpClient client = new HttpClient();
+        private static DateTime dtLastUpdate = DateTime.MinValue;
+        private static double lastPrice = 0;
+
+        public static string CoinGeckoAPI = "https://api.coingecko.com/api/v3/simple/price?ids=ergo&vs_currencies=usd";
+        public static int cacheResultsForXSeconds = 60 * 15;
+        public static async Task<double?> GetCurrentERGPriceInUSDCached()
+        {
+            if (lastPrice == 0 || (DateTime.Now - dtLastUpdate).TotalSeconds >= cacheResultsForXSeconds)
+            {
+                try
+                {
+                    CoinGeckoErgoPriceResult? ret = await client.GetFromJsonAsync<CoinGeckoErgoPriceResult>(CoinGeckoAPI);
+                    if (ret != null) lastPrice = ret?.ergo?.usd ?? 0;
+                }
+                catch (Exception e)
+                {
+
+                }
+                
+            }
+            return lastPrice;
+        }
+    }
+}
