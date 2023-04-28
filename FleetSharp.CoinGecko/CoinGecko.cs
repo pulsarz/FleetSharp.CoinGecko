eca607f9c3c6e534cae71c3c2b8e182dbb27d02c
pulsarz <nicksasa@gmail.com>
2023-04-28 11:14:05 +0200

    Make sure we only do 1 update when needed and not multiple when multiple calls get executed in the same timeframe + make all the other calls wait untill data is updated

--- FleetSharp.CoinGecko/CoinGecko.cs
+++ FleetSharp.CoinGecko/CoinGecko.cs
@@ -16,24 +16,33 @@ namespace FleetSharp.CoinGecko
         private static HttpClient client = new HttpClient();
         private static DateTime dtLastUpdate = DateTime.MinValue;
         private static double lastPrice = 0;
+        private static SemaphoreSlim sema = new SemaphoreSlim(1, 1);
 
         public static string CoinGeckoAPI = "https://api.coingecko.com/api/v3/simple/price?ids=ergo&vs_currencies=usd";
         public static int cacheResultsForXSeconds = 60 * 15;
         public static async Task<double?> GetCurrentERGPriceInUSDCached()
         {
+            
             if (lastPrice == 0 || (DateTime.Now - dtLastUpdate).TotalSeconds >= cacheResultsForXSeconds)
             {
-                try
+                await sema.WaitAsync();
+                //Do check again now that we are "locked"
+                if (lastPrice == 0 || (DateTime.Now - dtLastUpdate).TotalSeconds >= cacheResultsForXSeconds)
                 {
-                    CoinGeckoErgoPriceResult? ret = await client.GetFromJsonAsync<CoinGeckoErgoPriceResult>(CoinGeckoAPI);
-                    if (ret != null) lastPrice = ret?.ergo?.usd ?? 0;
-                }
-                catch (Exception e)
-                {
+                    try
+                    {
+                        CoinGeckoErgoPriceResult? ret = await client.GetFromJsonAsync<CoinGeckoErgoPriceResult>(CoinGeckoAPI);
+                        if (ret != null) lastPrice = ret?.ergo?.usd ?? 0;
+                    }
+                    catch (Exception e)
+                    {
 
+                    }
+                    dtLastUpdate = DateTime.Now;
                 }
-                dtLastUpdate = DateTime.Now;
+                sema.Release();
             }
+
             return lastPrice;
         }
     }
